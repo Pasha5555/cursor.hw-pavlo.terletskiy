43e0a65fcbfa4b47fd9fadbeda8f78dd73fdba1c
Pavlo <pavloterleckij2@gmail.com>
2020-02-26 22:44:43 +0200

    changed hw#7 and added hw#8

--- 07-hw-this/this.js
+++ 07-hw-this/this.js
@@ -2,32 +2,33 @@ const ukraine = { tax: 0.195, middleSalary: 1789, vacancies: 11476 };
 const latvia = { tax: 0.25, middleSalary: 1586, vacancies: 3921 };
 const litva = { tax: 0.15, middleSalary: 1509, vacancies: 1114 };
 
+const array =  [ukraine, latvia, litva];
+const eachCountry = func => (array.map(number=> func.call(number))).join(" / ");
 
+ //1.
 function getMyTaxes(salary){
     return salary * this.tax;
 }
-console.log(getMyTaxes.call(litva, 1000));
+console.log(getMyTaxes.call(litva, 1800));
 
-
+//2.
 function getMiddleTaxes(){
     return this.tax * this.middleSalary;
 }
-console.log(getMiddleTaxes.call(ukraine));
+console.log("Україна/Латвія/Литва:", eachCountry(getMiddleTaxes));
 
-
+//3.
 function getTotalTaxes(){
     return this.tax * this.middleSalary *this.vacancies;
 }
-console.log(getTotalTaxes.call(ukraine));
+console.log("Україна/Латвія/Литва:", eachCountry(getTotalTaxes));;
 
+//4.
 function getMySalary(){
     const salaryInfo = {};
     salaryInfo.salary = Math.floor(Math.random() *(2000-1500) + 1500);
-    salaryInfo.taxes = this.tax * salaryInfo.salary; 
-    salaryInfo.profit = salaryInfo.salary - salaryInfo.taxes;
+    salaryInfo.taxes = (this.tax * salaryInfo.salary).toFixed(2); 
+    salaryInfo.profit = (salaryInfo.salary - salaryInfo.taxes).toFixed(2);
     return salaryInfo;
 }
 setInterval(()=> console.log(getMySalary.call(ukraine)), 10000);
-
-
-
---
+++ 08-hw-classes/classes.js
@@ -0,0 +1,61 @@
+class Student{
+    constructor(university, course, fullName){
+        this.university = university;
+        this.course = course;
+        this.fullName = fullName;
+    }
+    marks = [5, 4, 4, 5];
+
+    getInfo(){
+        console.log(`Cтудент ${this.course}-го курсу університету ${this.university} м. Львів, ${this.fullName}`);
+    }
+
+    set setMark(numb){
+        if(this.marks === null){
+            return null;
+        }else return this.marks.push(numb);
+    }
+
+    get getMark(){
+        return this.marks;
+    }
+
+    getAverage(){
+        return (this.marks.reduce((sum, mark) => sum += mark) / this.marks.length);
+    }
+
+    dismiss(){
+        this.marks = null;
+        return this.marks;
+    }
+
+    recover(){
+        return this.marks = [5,4,4,5];
+    }
+
+}
+const student = new Student("Львівська політехніка", 4, "Terletskiy Pavlo");
+student.getInfo();
+student.setMark = 5;
+console.log(student.getMark);
+console.log("Середня оцінка", student.getAverage());
+console.log("Виключили студента, тому що оцінки -", student.dismiss());
+console.log("Відновили студента:", student.recover());
+
+class BudgetStudent extends Student{
+    constructor(university, course, fullName, scholarship){
+        super(university, course, fullName);
+        this.scholarship = scholarship;
+        setInterval(()=> {this.getScholarship()}, 30000);
+    }
+
+    getScholarship(){
+        if(this.marks !== null && this.getAverage() >= 4){
+            console.log("Ви отримали 1400 грн. степендії");
+        }
+    }
+}
+console.log("<-----------------Advanced------------------->");
+const budgetStudent = new BudgetStudent("LNU im.Ivana Franka", 3, "Novachok Stas");
+budgetStudent.getScholarship();
+budgetStudent.getInfo();
