5626d89a766a35b7dc45554901d4463491cffd1a
Pavlo <pavloterleckij2@gmail.com>
2020-03-10 23:36:02 +0200

    changed hw#10 events and added hw#11 promises

--- 10-hw-events/events.js
+++ 10-hw-events/events.js
@@ -1,5 +1,4 @@
-
-function Events(event) {
+function Keyboard(event) {
     const audio = document.getElementById(event.key.toUpperCase());
     const button = document.querySelector(`.${event.key.toUpperCase()}`);
     audio.currentTime = 0;
@@ -11,5 +10,22 @@ function Events(event) {
        button.classList.remove('doing')
     },500);
 }
+document.addEventListener('keydown', Keyboard);
 
-document.addEventListener('keydown', Events);
+
+const buttons = document.getElementsByTagName("button");
+const arrOfButtons = Array.from(buttons);
+arrOfButtons.forEach(click => {
+  click.addEventListener('click', function() {
+    const audio = new Audio();
+    audio.src = `audio/${click.id}.wav`;
+    audio.play();
+    audio.currentTime = 0;
+    click.classList.add('doing');
+        setTimeout(()=>audio.pause(), 3000);
+
+        setTimeout(()=>{
+            click.classList.remove('doing')
+        },500);
+  });
+});
